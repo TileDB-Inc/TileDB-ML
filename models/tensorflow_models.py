80fa77fe64605b6ac692faac3786714b5f5b5b4b
George Skoumas <george.skoumas@tiledb.com>
2021-01-26 21:38:26 +0200

    fixed weights and optimizer serialization

--- models/tensorflow_models.py
+++ models/tensorflow_models.py
@@ -1,29 +1,35 @@
 import tiledb
 import logging
-import numpy as np
 import pickle
 import json
+import numpy as np
 
 from tensorflow.keras import Model
-from base_model import TileDBModel
+from tensorflow.python.keras import optimizer_v1
 from tensorflow.python.keras.saving import saving_utils
 from tensorflow.python.keras.saving.saved_model import json_utils
+
+from base_model import TileDBModel
 
 TILEDB_CONTEXT = tiledb.Ctx()
 
 
 class TensorflowTileDB(TileDBModel):
 
-    def __init__(self, model: Model, **kwargs):
-        self.model = model
+    def __init__(self, model: Model, include_optimizer: bool, **kwargs):
         super(TensorflowTileDB, self).__init__(**kwargs)
+        self.model = model
+        self.include_optimizer = include_optimizer
 
-    def save(self, update=False, include_optimizer=True):
+        # Initialize model metadata and model weights
+        self.model_metadata = saving_utils.model_metadata(self.model, self.include_optimizer)
+        self.model_weights = model.get_weights()
+
+    def save(self, update=False):
         """
         Saves a Tensorflow model as a TileDB array
 
         :param update: Whether we should update any existing TileDB array model at the target location
-        :param include_optimizer: If True, save optimizer's state together
         """
 
         if len(self.model.weights) != len(self.model._undeduplicated_weights):
@@ -37,28 +43,14 @@ class TensorflowTileDB(TileDBModel):
             logging.warning('TileDB array already exists but update=False. Next time set update=True. Returning')
             return
 
-        # Get model metadata and weights
-        model_metadata = saving_utils.model_metadata(self.model, include_optimizer)
-        model_weights = self.model.get_weights()
-
-        # Serialize model weights
-        serialized_model_weights = [str(pickle.dumps(layer_weights)) for layer_weights in model_weights]
+        # Serialize models weights and optimizer (if needed)
+        self._serialize()
 
         # Create TileDB model array
         if not update:
-            self._create_array(len(model_weights))
+            self._create_array()
 
-        with tiledb.open(self.uri, 'w') as tf_model_tiledb:
-            # Insert weights
-            tf_model_tiledb[:] = {"weights": np.array(serialized_model_weights)}
-
-            # Insert all model metadata
-            for key, value in model_metadata.items():
-                if isinstance(value, (dict, list, tuple)):
-                    tf_model_tiledb.meta[key] = json.dumps(
-                        value, default=json_utils.get_json_type).encode('UTF-8')
-                else:
-                    tf_model_tiledb.meta[key] = value
+        self._write_array()
 
     def load(self):
         """
@@ -66,16 +58,15 @@ class TensorflowTileDB(TileDBModel):
         :return: Model. Tensorflow model
         """
 
-    def _create_array(self, domain_extent):
+    def _create_array(self):
         """
         Creates a TileDB array for a Tensorflow model
-        :param domain_extent: The extent of the domain of the 1st dimension, i.e, model layer index
         """
 
         dom = tiledb.Domain(
-            tiledb.Dim(name="layer_index",
-                       domain=(0, domain_extent - 1),
-                       tile=domain_extent,
+            tiledb.Dim(name="model",
+                       domain=(0, 0),
+                       tile=1,
                        dtype=np.int32
                        ),
             ctx=TILEDB_CONTEXT,
@@ -87,7 +78,13 @@ class TensorflowTileDB(TileDBModel):
                         var=True,
                         filters=tiledb.FilterList([tiledb.ZstdFilter()]),
                         ctx=TILEDB_CONTEXT
-                        )
+                        ),
+            tiledb.Attr(name="optimizer",
+                        dtype="U",
+                        var=True,
+                        filters=tiledb.FilterList([tiledb.ZstdFilter()]),
+                        ctx=TILEDB_CONTEXT
+                        ),
         ]
 
         schema = tiledb.ArraySchema(domain=dom,
@@ -97,3 +94,34 @@ class TensorflowTileDB(TileDBModel):
                                     )
 
         tiledb.Array.create(self.uri, schema, ctx=TILEDB_CONTEXT)
+
+    def _write_array(self):
+        """
+        Writes Tensorflow model to a TileDB array.
+        """
+
+        with tiledb.open(self.uri, 'w') as tf_model_tiledb:
+            # Insert weights
+            tf_model_tiledb[:] = {"weights": np.array([self.serialized_model_weights]),
+                                  "optimizer": np.array([self.serialized_optimizer_weights])}
+
+            # Insert all model metadata
+            for key, value in self.model_metadata.items():
+                if isinstance(value, (dict, list, tuple)):
+                    tf_model_tiledb.meta[key] = json.dumps(
+                        value, default=json_utils.get_json_type).encode('UTF-8')
+                else:
+                    tf_model_tiledb.meta[key] = value
+
+    def _serialize(self):
+        """
+        Serialization of model weights and optimizer
+        """
+
+        # Serialize model weights.
+        self.serialized_model_weights = str(pickle.dumps(self.model_weights, protocol=-1))
+
+        if self.include_optimizer and self.model.optimizer and \
+                not isinstance(self.model.optimizer, optimizer_v1.TFOptimizer):
+            optimizer_weights = getattr(self.model.optimizer, 'weights')
+            self.serialized_optimizer_weights = str(pickle.dumps(optimizer_weights, protocol=-1))
