d847d5c3e876e551492f994563d07b5313b5065e
George Skoumas <george.skoumas@tiledb.com>
2021-01-27 13:28:09 +0200

    added error handling, improved tf classs

--- models/base_model.py
+++ models/base_model.py
@@ -1,35 +1,36 @@
-from abc import ABC, abstractmethod
+import abc
 
 
-class TileDBModel(ABC):
-    def __init__(self, uri: str, input_shape: tuple, output_shape: tuple, description: str):
+class TileDBModel(abc.ABC):
+    """
+    This is the base class for all TileDB model storage functionalities, i.e,
+    store machine learning models (Tensorflow, PyTorch, ect) as TileDB arrays.
+    """
+    def __init__(self, uri: str, description: str):
         """
-        Base class for saving machine learning models as TileDB arrays and loading machine learning models from TileDB
-        arrays.
+        Base class for saving machine learning models as TileDB arrays
+        and loading machine learning models from TileDB arrays.
         :param uri: str
-        :param input_shape: tuple
-        :param output_shape: tuple
         :param description: str
         """
         self.uri = uri
-        self.input_shape = input_shape
-        self.output_shape = output_shape
         self.description = description
         super().__init__()
 
-    @abstractmethod
+    @abc.abstractmethod
     def save(self):
         """
-        Abstract method that saves a machine learning model as a TileDB array. Must be implemented per machine learning
-        framework, i.e, Tensorflow, PyTorch etc.
+        Abstract method that saves a machine learning model as a TileDB array.
+        Must be implemented per machine learning framework, i.e, Tensorflow,
+        PyTorch etc.
         """
         raise NotImplementedError("Please implement save method!")
 
-    @abstractmethod
+    @abc.abstractmethod
     def load(self):
         """
-        Abstract method that loads a machine learning model from a model TileDB array. Must be implemented per machine
-        learning framework.
+        Abstract method that loads a machine learning model from a model TileDB array.
+        Must be implemented per machine learning framework.
         :return: Model
             Could be a Tensorflow or PyTorch model
         """
--- models/tensorflow_models.py
+++ models/tensorflow_models.py
@@ -1,8 +1,9 @@
-import tiledb
 import logging
 import pickle
 import json
+from urllib.error import HTTPError
 import numpy as np
+import tiledb
 
 from tensorflow.keras import Model
 from tensorflow.python.keras import optimizer_v1
@@ -15,36 +16,37 @@ TILEDB_CONTEXT = tiledb.Ctx()
 
 
 class TensorflowTileDB(TileDBModel):
+    """
+    Class that implements all functionality needed to save Tensorflow models as
+    TileDB arrays and load Tensorflow models from TileDB arrays.
 
+    """
     def __init__(self, model: Model, include_optimizer: bool, **kwargs):
-        super(TensorflowTileDB, self).__init__(**kwargs)
         self.model = model
         self.include_optimizer = include_optimizer
+        self.serialized_model_weights = None
+        self.serialized_optimizer = None
 
-        # Initialize model metadata and model weights
-        self.model_metadata = saving_utils.model_metadata(self.model, self.include_optimizer)
-        self.model_weights = model.get_weights()
+        super(TensorflowTileDB, self).__init__(**kwargs)
 
     def save(self, update=False):
         """
         Saves a Tensorflow model as a TileDB array
-
-        :param update: Whether we should update any existing TileDB array model at the target location
+        :param update: Whether we should update any existing TileDB array
+        model at the target location
         """
-
         if len(self.model.weights) != len(self.model._undeduplicated_weights):
             logging.warning('Found duplicated `Variable`s in Model\'s `weights`. '
                             'This is usually caused by `Variable`s being shared by '
                             'Layers in the Model. These `Variable`s will be treated '
                             'as separate `Variable`s when the Model is restored.')
 
-        # Check if the array already exists but update is False.
-        if tiledb.object_type(self.uri) == "array" and update is False:
-            logging.warning('TileDB array already exists but update=False. Next time set update=True. Returning')
-            return
-
         # Serialize models weights and optimizer (if needed)
-        self._serialize()
+        self._serialize_model_weights()
+
+        # Serialize model optimizer
+        if self.include_optimizer:
+            self._serialize_model_optimizer()
 
         # Create TileDB model array
         if not update:
@@ -63,37 +65,53 @@ class TensorflowTileDB(TileDBModel):
         Creates a TileDB array for a Tensorflow model
         """
 
-        dom = tiledb.Domain(
-            tiledb.Dim(name="model",
-                       domain=(0, 0),
-                       tile=1,
-                       dtype=np.int32
-                       ),
-            ctx=TILEDB_CONTEXT,
-        )
+        try:
+            dom = tiledb.Domain(
+                tiledb.Dim(name="model",
+                           domain=(0, 0),
+                           tile=1,
+                           dtype=np.int32
+                           ),
+                ctx=TILEDB_CONTEXT,
+            )
 
-        attrs = [
-            tiledb.Attr(name="weights",
-                        dtype="U",
-                        var=True,
-                        filters=tiledb.FilterList([tiledb.ZstdFilter()]),
-                        ctx=TILEDB_CONTEXT
-                        ),
-            tiledb.Attr(name="optimizer",
-                        dtype="U",
-                        var=True,
-                        filters=tiledb.FilterList([tiledb.ZstdFilter()]),
-                        ctx=TILEDB_CONTEXT
-                        ),
-        ]
+            attrs = [
+                tiledb.Attr(name="weights",
+                            dtype="U",
+                            var=True,
+                            filters=tiledb.FilterList([tiledb.ZstdFilter()]),
+                            ctx=TILEDB_CONTEXT
+                            ),
+                tiledb.Attr(name="optimizer",
+                            dtype="U",
+                            var=True,
+                            filters=tiledb.FilterList([tiledb.ZstdFilter()]),
+                            ctx=TILEDB_CONTEXT
+                            ),
+            ]
 
-        schema = tiledb.ArraySchema(domain=dom,
-                                    sparse=False,
-                                    attrs=attrs,
-                                    ctx=TILEDB_CONTEXT
-                                    )
+            schema = tiledb.ArraySchema(domain=dom,
+                                        sparse=False,
+                                        attrs=attrs,
+                                        ctx=TILEDB_CONTEXT
+                                        )
 
-        tiledb.Array.create(self.uri, schema, ctx=TILEDB_CONTEXT)
+            tiledb.Array.create(self.uri, schema, ctx=TILEDB_CONTEXT)
+        except tiledb.TileDBError as error:
+            if "Error while listing with prefix" in str(error):
+                # It is possible to land here if user sets wrong default s3 credentials
+                # with respect to default s3 path
+                raise HTTPError(code=400, msg="Error creating file, %s Are your S3 "
+                                              "credentials valid?" % str(error))
+
+            if "already exists" in str(error):
+                logging.warning('TileDB array already exists but update=False. '
+                                'Next time set update=True. Returning')
+                return
+        except HTTPError as error:
+            raise error
+        except Exception as error:
+            raise HTTPError(code=400, msg="Error creating file %s " % str(error))
 
     def _write_array(self):
         """
@@ -103,25 +121,29 @@ class TensorflowTileDB(TileDBModel):
         with tiledb.open(self.uri, 'w') as tf_model_tiledb:
             # Insert weights
             tf_model_tiledb[:] = {"weights": np.array([self.serialized_model_weights]),
-                                  "optimizer": np.array([self.serialized_optimizer_weights])}
+                                  "optimizer": np.array([self.serialized_optimizer])}
 
             # Insert all model metadata
-            for key, value in self.model_metadata.items():
+            model_metadata = saving_utils.model_metadata(self.model, self.include_optimizer)
+            for key, value in model_metadata.items():
                 if isinstance(value, (dict, list, tuple)):
                     tf_model_tiledb.meta[key] = json.dumps(
                         value, default=json_utils.get_json_type).encode('UTF-8')
                 else:
                     tf_model_tiledb.meta[key] = value
 
-    def _serialize(self):
+    def _serialize_model_weights(self):
         """
-        Serialization of model weights and optimizer
+        Serialization of model weights
         """
-
         # Serialize model weights.
-        self.serialized_model_weights = str(pickle.dumps(self.model_weights, protocol=-1))
+        self.serialized_model_weights = str(pickle.dumps(self.model.get_weights(), protocol=-1))
 
+    def _serialize_model_optimizer(self):
+        """
+        Serialization of model optimizer
+        """
         if self.include_optimizer and self.model.optimizer and \
                 not isinstance(self.model.optimizer, optimizer_v1.TFOptimizer):
             optimizer_weights = getattr(self.model.optimizer, 'weights')
-            self.serialized_optimizer_weights = str(pickle.dumps(optimizer_weights, protocol=-1))
+            self.serialized_optimizer = str(pickle.dumps(optimizer_weights, protocol=-1))
