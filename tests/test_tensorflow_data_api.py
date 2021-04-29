9b7ce487e6288fddb9a730d13927dfaba5c06d20
George Skoumas <george.skoumas@tiledb.com>
2021-04-29 22:35:46 +0300

    TileDB integration with Tensorflow Data API (#25)
    * tensorflow data api integration class
    
    * added output_signature to tf.data.Dataset.from_generator declation
    
    * added tests for tiledb integration with tf data api
    
    * PR changes, tests and x,y equal rows check
    
    * tiledb tf data api class gets open tiledb arrays

---
+++ tests/test_tensorflow_data_api.py
@@ -0,0 +1,140 @@
+"""Tests for TileDB integration with Tensorflow Data API."""
+
+import os
+import tiledb
+import numpy as np
+import uuid
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import (
+    Dense,
+    Flatten,
+    Dropout,
+)
+from tensorflow.python.keras import testing_utils
+from tensorflow.python.platform import test
+
+from tiledb.ml.data_apis.tensorflow import TensorflowTileDBDenseDataset
+
+# Suppress all Tensorflow messages
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+# Test parameters
+NUM_OF_CLASSES = 5
+BATCH_SIZE = 32
+ROWS = 1000
+
+# We test for 2d, 3d, 4d and 5d data
+INPUT_SHAPES = [(10,), (10, 3), (10, 10, 3), (10, 10, 10, 3)]
+
+
+def ingest_in_tiledb(data: np.array, batch_size: int, uri: str):
+    dims = [
+        tiledb.Dim(
+            name="dim_" + str(dim),
+            domain=(0, data.shape[dim] - 1),
+            tile=data.shape[dim] if dim > 0 else batch_size,
+            dtype=np.int32,
+        )
+        for dim in range(data.ndim)
+    ]
+
+    # TileDB schema
+    schema = tiledb.ArraySchema(
+        domain=tiledb.Domain(*dims),
+        sparse=False,
+        attrs=[tiledb.Attr(name="features", dtype=np.float32)],
+    )
+    # Create array
+    tiledb.Array.create(uri, schema)
+
+    # Ingest
+    with tiledb.open(uri, "w") as tiledb_array:
+        tiledb_array[:] = {"features": data}
+
+
+def create_model(input_shape: tuple, num_of_classes: int):
+    model = Sequential()
+    model.add(tf.keras.Input(shape=input_shape))
+    model.add(Flatten())
+    model.add(Dense(64))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_of_classes))
+
+    model.compile(
+        loss="categorical_crossentropy", optimizer="rmsprop", metrics=["accuracy"]
+    )
+
+    return model
+
+
+class TestTileDBTensorflowDataAPI(test.TestCase):
+    @testing_utils.run_v2_only
+    def test_tiledb_tf_data_api_with_multiple_dim_data(self):
+        for input_shape in INPUT_SHAPES:
+            with self.subTest():
+                model = create_model(
+                    input_shape=input_shape, num_of_classes=NUM_OF_CLASSES
+                )
+
+                array_uuid = str(uuid.uuid4())
+                tiledb_uri_x = os.path.join(self.get_temp_dir(), "x" + array_uuid)
+                tiledb_uri_y = os.path.join(self.get_temp_dir(), "y" + array_uuid)
+
+                dataset_shape_x = (ROWS,) + input_shape
+                dataset_shape_y = (ROWS, NUM_OF_CLASSES)
+
+                ingest_in_tiledb(
+                    uri=tiledb_uri_x,
+                    data=np.random.rand(*dataset_shape_x),
+                    batch_size=BATCH_SIZE,
+                )
+                ingest_in_tiledb(
+                    uri=tiledb_uri_y,
+                    data=np.random.rand(*dataset_shape_y),
+                    batch_size=BATCH_SIZE,
+                )
+
+                with tiledb.DenseArray(tiledb_uri_x, mode="r") as x, tiledb.DenseArray(
+                    tiledb_uri_y, mode="r"
+                ) as y:
+
+                    tiledb_dataset = TensorflowTileDBDenseDataset(
+                        x_array=x, y_array=y, batch_size=BATCH_SIZE
+                    )
+
+                    self.assertIsInstance(tiledb_dataset, tf.data.Dataset)
+
+                    model.fit(tiledb_dataset, verbose=0, epochs=1)
+
+    @testing_utils.run_v2_only
+    def test_except_with_diff_number_of_x_y_rows(self):
+        array_uuid = str(uuid.uuid4())
+        tiledb_uri_x = os.path.join(self.get_temp_dir(), "x" + array_uuid)
+        tiledb_uri_y = os.path.join(self.get_temp_dir(), "y" + array_uuid)
+
+        # Add one extra row on X
+        dataset_shape_x = (ROWS + 1,) + INPUT_SHAPES[0]
+        dataset_shape_y = (ROWS, NUM_OF_CLASSES)
+
+        ingest_in_tiledb(
+            uri=tiledb_uri_x,
+            data=np.random.rand(*dataset_shape_x),
+            batch_size=BATCH_SIZE,
+        )
+        ingest_in_tiledb(
+            uri=tiledb_uri_y,
+            data=np.random.rand(*dataset_shape_y),
+            batch_size=BATCH_SIZE,
+        )
+
+        with tiledb.DenseArray(tiledb_uri_x, mode="r") as x, tiledb.DenseArray(
+            tiledb_uri_y, mode="r"
+        ) as y:
+            with self.assertRaises(Exception):
+                TensorflowTileDBDenseDataset(
+                    x_array=x, y_array=y, batch_size=BATCH_SIZE
+                )
---
+++ tiledb/ml/data_apis/tensorflow.py
@@ -0,0 +1,69 @@
+"""Functionality for loading data directly from TileDB arrays into the Tensorflow Data API."""
+import tiledb
+import tensorflow as tf
+
+
+class TensorflowTileDBDenseDataset(tf.data.Dataset):
+    """
+    Class that implements all functionality needed to load data from TileDB directly to the
+    Tensorflow Data API, by employing generators.
+    """
+
+    def __new__(cls, x_array: tiledb.Array, y_array: tiledb.Array, batch_size: int):
+        """
+        Returns a Tensorflow Dataset object which loads data from TileDB arrays by employing a generator.
+        :param x_array: TileDB Dense Array. Array that contains features.
+        :param y_array: TileDB Dense Array. Array that contains labels.
+        :param batch_size: Integer. The size of the batch that the implemented _generator method will return.
+        For optimal reads from a TileDB array, it is recommended to set the batch size equal to the tile extent of the
+        dimension we query (here, we always query the first dimension of a TileDB array) in order to get a slice (batch)
+        of the data. For example, in case the tile extent of the first dimension of a TileDB array (x or y) is equal to
+        32, it's recommended to set batch_size=32. Any batch size will work, but in case it's not equal the tile extent
+        of the first dimension of the TileDB array, you won't achieve highest read speed. For more details on tiles,
+        tile extent and indices in TileDB, please check here:
+        https://docs.tiledb.com/main/solutions/tiledb-embedded/performance-tips/choosing-tiling-and-cell-layout#dense-arrays
+        """
+
+        # Check that x and y have the same number of rows
+        if x_array.schema.domain.shape[0] != y_array.schema.domain.shape[0]:
+            raise Exception(
+                "X and Y should have the same number of rows, i.e., the 1st dimension "
+                "of TileDB arrays X, Y should be of equal domain extent."
+            )
+
+        # Get number of observations
+        rows = x_array.schema.domain.shape[0]
+
+        # Get x and y shapes
+        x_shape = (None,) + x_array.schema.domain.shape[1:]
+        y_shape = (None,) + y_array.schema.domain.shape[1:]
+
+        # Get x and y data types
+        x_dtype = x_array.schema.attr(0).dtype
+        y_dtype = y_array.schema.attr(0).dtype
+
+        return tf.data.Dataset.from_generator(
+            generator=cls._generator,
+            output_signature=(
+                tf.TensorSpec(shape=x_shape, dtype=x_dtype),
+                tf.TensorSpec(shape=y_shape, dtype=y_dtype),
+            ),
+            args=(x_array, y_array, rows, batch_size),
+        )
+
+    @staticmethod
+    def _generator(
+        x: tiledb.Array, y: tiledb.Array, rows: int, batch_size: int
+    ) -> tuple:
+        """
+        A generator function that yields the next training batch.
+        :param x: TileDB array. An opened TileDB array which contains features.
+        :param y: TileDB array. An opened TileDB array which contains labels.
+        :param rows: Integer. The number of observations in x, y datasets.
+        :param batch_size: Integer. Size of batch, i.e., number of rows returned per call.
+        :return: Tuple. Tuple that contains x and y batches.
+        """
+        # Loop over batches
+        for offset in range(0, rows, batch_size):
+            # Yield the next training batch
+            yield x[offset : offset + batch_size], y[offset : offset + batch_size]
