df91b1e1327143b3cbf74410a01be5ad8c7ac17d
ktsitsi <kostas.t@tiledb.com>
2021-05-13 14:29:24 +0300

    Adding extra restriction for the size of sparse array batches

--- tests/test_tensorflow_sparse_data_api.py
+++ tests/test_tensorflow_sparse_data_api.py
@@ -1,45 +1,56 @@
 """Tests for TileDB integration with Tensorflow Data API."""
 
 import os
+
+import numpy
 import tiledb
 import numpy as np
 import uuid
 from scipy.sparse import random
 from scipy import stats
-import sparse
 
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import (
-    Dense,
-    Flatten,
-    Dropout,
-)
+
 from tensorflow.python.keras import testing_utils
 from tensorflow.python.platform import test
 
 from tiledb.ml.data_apis.tensorflow_sparse import TensorflowTileDBSparseDataset
 
-
 # Suppress all Tensorflow messages
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
-tf.compat.v1.enable_eager_execution()
+# tf.compat.v1.enable_eager_execution()
 
 # Test parameters
 NUM_OF_CLASSES = 1
 BATCH_SIZE = 32
 ROWS = 1000
 
-# We test for 2d, 3d, 4d and 5d data
-INPUT_SHAPES = [(10,),]
+# We test for 2d
+INPUT_SHAPES = [(10,), ]
 
 
-class CustomRandomState(np.random.RandomState):
-    def randint(self, k):
-        i = np.random.randint(k)
-        return i - i % 2
+def create_sparse_array_one_hot_2d(dims: tuple):
+    seed = np.random.randint(low=0, high=dims[1][0], size=(dims[0],))
+    seed[-1] = dims[1][0] - 1
+    a = np.array(seed)
+    b = np.zeros((a.size, a.max() + 1))
+    b[np.arange(a.size), a] = 1
+    return b
+
+# def create_sparse_array_one_hot_nd(size: tuple, axis=-1, dtype=bool):
+#     a = x = np.random.randint(5, size=(3, 2))
+#     pos = axis if axis >= 0 else a.ndim + axis + 1
+#     shape = list(a.shape)
+#     shape.insert(pos, a.max() + 1)
+#     out = np.zeros(shape, dtype)
+#     ind = list(np.indices(a.shape, sparse=True))
+#     ind.insert(pos, a)
+#     out[tuple(ind)] = True
+#     return out
+
 
 def ingest_in_tiledb(data: np.array, batch_size: int, uri: str):
     dims = [
@@ -63,7 +74,7 @@ def ingest_in_tiledb(data: np.array, batch_size: int, uri: str):
 
     # Ingest
     with tiledb.open(uri, "w") as tiledb_array:
-        tiledb_array[:] = data
+        tiledb_array[:] = {"features": data}
 
 def ingest_in_tiledb_sparse(data: np.array, batch_size: int, uri: str):
     dims = [
@@ -88,15 +99,14 @@ def ingest_in_tiledb_sparse(data: np.array, batch_size: int, uri: str):
 
     # Ingest
     with tiledb.open(uri, "w") as tiledb_array:
-        I, J = data.row, data.col
-        data_elem = np.array(data.data)
-        tiledb_array[I, J] = data_elem
+        idx = data.nonzero()
+        I, J = idx[0], idx[1]
+        tiledb_array[I, J] = data[np.nonzero(data)]
 
 
-def create_model(input_shape: tuple, num_of_classes: int):
-
+def create_model(input_shape: tuple, num_of_classes: int) -> object:
     model = Sequential()
-    model.add(tf.keras.Input(shape=input_shape))
+    model.add(tf.keras.Input(shape=input_shape, sparse=True))
     model.compile()
 
     return model
@@ -104,7 +114,7 @@ def create_model(input_shape: tuple, num_of_classes: int):
 
 class TestTileDBTensorflowSparseDataAPI(test.TestCase):
     @testing_utils.run_v2_only
-    def test_sparse_tiledb_tf_data_api_with_multiple_dim_data(self):
+    def test_tiledb_tf_sparse_data_api_with_with_sparse_data_sparse_label(self):
         for input_shape in INPUT_SHAPES:
             with self.subTest():
                 model = create_model(
@@ -115,20 +125,17 @@ class TestTileDBTensorflowSparseDataAPI(test.TestCase):
                 tiledb_uri_x = os.path.join(self.get_temp_dir(), "x" + array_uuid)
                 tiledb_uri_y = os.path.join(self.get_temp_dir(), "y" + array_uuid)
 
-                dataset_shape_x = (ROWS,) + input_shape
-                dataset_shape_y = (ROWS, NUM_OF_CLASSES)
-
-                rs = CustomRandomState()
-                rvs = stats.poisson(25, loc=10).rvs
+                dataset_shape_x = (ROWS, input_shape)
+                dataset_shape_y = (ROWS, (NUM_OF_CLASSES,))
 
                 ingest_in_tiledb_sparse(
                     uri=tiledb_uri_x,
-                    data=random(*dataset_shape_x, density=0.25, random_state=rs, data_rvs=rvs),
+                    data=create_sparse_array_one_hot_2d(dataset_shape_x),
                     batch_size=BATCH_SIZE,
                 )
                 ingest_in_tiledb_sparse(
                     uri=tiledb_uri_y,
-                    data=random(*dataset_shape_y, density=0.25, random_state=rs, data_rvs=rvs),
+                    data=create_sparse_array_one_hot_2d(dataset_shape_y),
                     batch_size=BATCH_SIZE,
                 )
 
@@ -140,9 +147,80 @@ class TestTileDBTensorflowSparseDataAPI(test.TestCase):
                     )
 
                     self.assertIsInstance(tiledb_dataset, tf.data.Dataset)
+                    model.fit(tiledb_dataset, epochs=2)
+
+    @testing_utils.run_v2_only
+    def test_tiledb_tf_sparse_data_api_with_sparse_data_dense_label(self):
+        for input_shape in INPUT_SHAPES:
+            with self.subTest():
+                model = create_model(
+                    input_shape=input_shape, num_of_classes=NUM_OF_CLASSES
+                )
+
+                array_uuid = str(uuid.uuid4())
+                tiledb_uri_x = os.path.join(self.get_temp_dir(), "x" + array_uuid)
+                tiledb_uri_y = os.path.join(self.get_temp_dir(), "y" + array_uuid)
+
+                dataset_shape_x = (ROWS, input_shape)
+                dataset_shape_y = (ROWS, NUM_OF_CLASSES)
 
 
-                    model.fit(tiledb_dataset, verbose=0, epochs=1)
+                ingest_in_tiledb_sparse(
+                    uri=tiledb_uri_x,
+                    data=create_sparse_array_one_hot_2d(dataset_shape_x),
+                    batch_size=BATCH_SIZE,
+                )
+                ingest_in_tiledb(
+                    uri=tiledb_uri_y,
+                    data=np.random.rand(*dataset_shape_y),
+                    batch_size=BATCH_SIZE,
+                )
+
+                with tiledb.SparseArray(tiledb_uri_x, mode="r") as x, tiledb.DenseArray(
+                        tiledb_uri_y, mode="r"
+                ) as y:
+                    tiledb_dataset = TensorflowTileDBSparseDataset(
+                        x_array=x, y_array=y, batch_size=BATCH_SIZE
+                    )
+
+                    self.assertIsInstance(tiledb_dataset, tf.data.Dataset)
+                    model.fit(tiledb_dataset, epochs=2)
+
+    @testing_utils.run_v2_only
+    def test_tiledb_tf_sparse_data_api_with_sparse_data_diff_number_of_batch_x_y_rows(self):
+        for input_shape in INPUT_SHAPES:
+            with self.subTest():
+
+                array_uuid = str(uuid.uuid4())
+                tiledb_uri_x = os.path.join(self.get_temp_dir(), "x" + array_uuid)
+                tiledb_uri_y = os.path.join(self.get_temp_dir(), "y" + array_uuid)
+
+                dataset_shape_x = (ROWS, input_shape)
+                dataset_shape_y = (ROWS, NUM_OF_CLASSES)
+
+                # Empty one random row
+                spoiled_data = create_sparse_array_one_hot_2d(dataset_shape_x)
+                spoiled_data[np.nonzero(spoiled_data[0])] = 0
+
+                ingest_in_tiledb_sparse(
+                    uri=tiledb_uri_x,
+                    data=spoiled_data,
+                    batch_size=BATCH_SIZE,
+                )
+                ingest_in_tiledb(
+                    uri=tiledb_uri_y,
+                    data=np.random.rand(*dataset_shape_y),
+                    batch_size=BATCH_SIZE,
+                )
+
+                with tiledb.SparseArray(tiledb_uri_x, mode="r") as x, tiledb.DenseArray(
+                        tiledb_uri_y, mode="r"
+                ) as y:
+                    tiledb_dataset = TensorflowTileDBSparseDataset(
+                        x_array=x, y_array=y, batch_size=BATCH_SIZE
+                    )
+
+                    self.assertRaises(Exception)
 
     @testing_utils.run_v2_only
     def test_sparse_except_with_diff_number_of_x_y_rows(self):
@@ -151,24 +229,21 @@ class TestTileDBTensorflowSparseDataAPI(test.TestCase):
         tiledb_uri_y = os.path.join(self.get_temp_dir(), "y" + array_uuid)
 
         # Add one extra row on X
-        dataset_shape_x = (ROWS + 1,) + INPUT_SHAPES[0]
-        dataset_shape_y = (ROWS, NUM_OF_CLASSES)
-
-        rs = CustomRandomState()
-        rvs = stats.poisson(25, loc=10).rvs
+        dataset_shape_x = (ROWS + 1, INPUT_SHAPES[0])
+        dataset_shape_y = (ROWS, (NUM_OF_CLASSES,))
 
         ingest_in_tiledb_sparse(
             uri=tiledb_uri_x,
-            data=random(*dataset_shape_x, density=0.25, random_state=rs, data_rvs=rvs),
+            data=create_sparse_array_one_hot_2d(dataset_shape_x),
             batch_size=BATCH_SIZE,
         )
-        ingest_in_tiledb(
+        ingest_in_tiledb_sparse(
             uri=tiledb_uri_y,
-            data=np.random.rand(*dataset_shape_y),
+            data=create_sparse_array_one_hot_2d(dataset_shape_y),
             batch_size=BATCH_SIZE,
         )
 
-        with tiledb.SparseArray(tiledb_uri_x, mode="r") as x, tiledb.DenseArray(
+        with tiledb.SparseArray(tiledb_uri_x, mode="r") as x, tiledb.SparseArray(
                 tiledb_uri_y, mode="r"
         ) as y:
             with self.assertRaises(Exception):
--- tiledb/ml/data_apis/tensorflow_sparse.py
+++ tiledb/ml/data_apis/tensorflow_sparse.py
@@ -7,8 +7,6 @@ import numpy as np
 from tensorflow.python.framework import constant_op
 from tensorflow.python.data.ops import dataset_ops
 
-tf.compat.v1.disable_eager_execution()
-
 
 class TensorflowTileDBSparseDataset(tf.data.Dataset):
     """
@@ -20,7 +18,7 @@ class TensorflowTileDBSparseDataset(tf.data.Dataset):
         """
         Returns a Tensorflow Dataset object which loads data from TileDB arrays by employing a generator.
         :param x_array: TileDB Sparse Array. Array that contains features.
-        :param y_array: TileDB Sparse Array. Array that contains labels.
+        :param y_array: TileDB Dense/Sparse Array. Array that contains labels.
         :param batch_size: Integer. The size of the batch that the implemented _generator method will return.
         For optimal reads from a TileDB array, it is recommended to set the batch size equal to the tile extent of the
         dimension we query (here, we always query the first dimension of a TileDB array) in order to get a slice (batch)
@@ -51,17 +49,27 @@ class TensorflowTileDBSparseDataset(tf.data.Dataset):
         cls.x_dtype = x_array.schema.attr(0).dtype
         cls.y_dtype = y_array.schema.attr(0).dtype
 
-        return dataset_ops.Dataset.from_generator(
-            generator=cls._generator,
-            output_signature=(
-                tf.SparseTensorSpec(shape=cls.x_shape, dtype=cls.x_dtype),
-                tf.SparseTensorSpec(shape=cls.y_shape, dtype=cls.y_dtype)
-            ),
-            args=(rows, batch_size)
-        )
+        if isinstance(y_array, tiledb.SparseArray):
+            return dataset_ops.Dataset.from_generator(
+                generator=cls._generator_sparse_sparse,
+                output_signature=(
+                    tf.SparseTensorSpec(shape=cls.x_shape, dtype=cls.x_dtype),
+                    tf.SparseTensorSpec(shape=cls.y_shape, dtype=cls.y_dtype)
+                ),
+                args=(rows, batch_size)
+            )
+        else:
+            return dataset_ops.Dataset.from_generator(
+                generator=cls._generator_sparse_dense,
+                output_signature=(
+                    tf.SparseTensorSpec(shape=cls.x_shape, dtype=cls.x_dtype),
+                    tf.TensorSpec(shape=cls.y_shape, dtype=cls.y_dtype)
+                ),
+                args=(rows, batch_size)
+            )
 
     @classmethod
-    def _generator(cls, rows: int, batch_size: int) -> tuple:
+    def _generator_sparse_sparse(cls, rows: int, batch_size: int) -> tuple:
         """
         A generator function that yields the next training batch.
         :param x: TileDB array. An opened TileDB array which contains features.
@@ -73,21 +81,29 @@ class TensorflowTileDBSparseDataset(tf.data.Dataset):
         # Loop over batches
         # https://github.com/tensorflow/tensorflow/issues/44565
         for offset in range(0, rows, batch_size):
-            values_y = list(cls.y[offset: offset + batch_size].items())[0][1]
-            values_x = list(cls.x[offset: offset + batch_size].items())[0][1]
+            y_batch = cls.y[offset: offset + batch_size]
+            x_batch = cls.x[offset: offset + batch_size]
+            values_y = list(y_batch.items())[0][1]
+            values_x = list(x_batch.items())[0][1]
 
             y_data = np.array(values_y).flatten()
             y_coords = []
             for i in range(0, cls.y.schema.domain.ndim):
                 dim_name = cls.y.schema.domain.dim(i).name
-                y_coords.append(np.array(cls.y[offset: offset + batch_size][dim_name]))
+                y_coords.append(np.array(y_batch[dim_name]))
 
             x_coords = []
             for i in range(0, cls.x.schema.domain.ndim):
                 dim_name = cls.x.schema.domain.dim(i).name
-                x_coords.append(np.array(cls.x[offset: offset + batch_size][dim_name]))
+                x_coords.append(np.array(x_batch[dim_name]))
 
             x_data = np.array(values_x).flatten()
+
+            if x_data.shape[0] != y_data.shape[0]:
+                raise Exception(
+                    "X and Y should have the same number of rows, i.e., the 1st dimension "
+                    "of TileDB arrays X, Y should be of equal domain extent inside the batch"
+                )
 
             yield tf.sparse.SparseTensor(indices=constant_op.constant(list(zip(*x_coords)), dtype=tf.int64),
                                          values=constant_op.constant(x_data, dtype=tf.float32),
@@ -95,3 +111,42 @@ class TensorflowTileDBSparseDataset(tf.data.Dataset):
                   tf.sparse.SparseTensor(indices=constant_op.constant(list(zip(*y_coords)), dtype=tf.int64),
                                          values=constant_op.constant(y_data, dtype=cls.y_dtype),
                                          dense_shape=(batch_size, cls.y_shape[1]))
+
+    @classmethod
+    def _generator_sparse_dense(cls, rows: int, batch_size: int) -> tuple:
+        """
+        A generator function that yields the next training batch.
+        :param x: TileDB array. An opened TileDB array which contains features.
+        :param y: TileDB array. An opened TileDB array which contains labels.
+        :param rows: Integer. The number of observations in x, y datasets.
+        :param batch_size: Integer. Size of batch, i.e., number of rows returned per call.
+        :return: Tuple. Tuple that contains x and y batches.
+        """
+        # Loop over batches
+        # https://github.com/tensorflow/tensorflow/issues/44565
+        for offset in range(0, rows, batch_size):
+            y_batch = cls.y[offset: offset + batch_size]
+            x_batch = cls.x[offset: offset + batch_size]
+
+            #TODO: Both for dense case support multiple attributes
+            values_y = list(y_batch.items())[0][1]
+            values_x = list(x_batch.items())[0][1]
+
+            x_coords = []
+            for i in range(0, cls.x.schema.domain.ndim):
+                dim_name = cls.x.schema.domain.dim(i).name
+                x_coords.append(np.array(x_batch[dim_name]))
+
+            x_data = np.array(values_x).flatten()
+
+            x_sparse_real_batch_size = x_coords[0].max(axis=0) - x_coords[0].min(axis=0)
+            if values_y.shape[0] - x_sparse_real_batch_size != 1:
+                raise Exception(
+                    "X and Y should have the same number of rows, i.e., the 1st dimension "
+                    "of TileDB arrays X, Y should be of equal domain extent inside the batch"
+                )
+
+            yield tf.sparse.SparseTensor(indices=constant_op.constant(list(zip(*x_coords)), dtype=tf.int64),
+                                         values=constant_op.constant(x_data, dtype=tf.float32),
+                                         dense_shape=(batch_size, cls.x_shape[1])), \
+                  values_y
