e7d2a3132441f75228a27841461b17da7975fe09
Andreas Ntalakas <antalakas@gmail.com>
2021-06-11 17:42:07 +0300

    Add cloud operations to ML Models (#46)
    * register, update, list ml models
    
    Rebase on master for optional dependencies changes.
    
    * black & flake
    
    * changed base model, cloud utils and sklearn model for TileDB-Cloud functionality
    
    * improved docstrings
    
    * fixed global variables
    
    * cloud utils, base class and sklearn class refactor. example notebook added. example files removed.
    
    * added extra file properties, updated readme, added tiledb-cloud as optional dependency
    
    Rebase to master
    
    * update model base class docstring
    
    * PR changes
    
    * added contenxt to sklearn model class
    
    Co-authored-by: George Skoumas <george.skoumas@tiledb.com>

--- tiledb/ml/models/base.py
+++ tiledb/ml/models/base.py
@@ -1,21 +1,47 @@
 """Base Class for saving and loading machine learning models."""
 
 import abc
+import os
+import tiledb.cloud
+
+from tiledb.ml._cloud_utils import get_s3_prefix
 
 
 class TileDBModel(abc.ABC):
     """
     This is the base class for all TileDB model storage functionalities, i.e,
-    store machine learning models (Tensorflow, PyTorch, ect) as TileDB arrays.
+    store machine learning models (Tensorflow, PyTorch, etc) as TileDB arrays.
     """
 
-    def __init__(self, uri: str):
+    def __init__(self, uri: str, namespace: str = None, ctx: tiledb.Ctx = None):
         """
         Base class for saving machine learning models as TileDB arrays
-        and loading machine learning models from TileDB arrays.
-        :param uri: str
+        and loading machine learning models from TileDB arrays. In case we need to interact
+        with TileDB-Cloud we have to pass user's TileDB-Cloud namespace. If we don't
+        models will be saved locally.
+        :param uri: str. TileDB array uri
+        :param namespace: str. In case we want to interact (save, load, update, check) with models on
+        TileDB-Cloud we need the user's namespace on TileDB-Cloud. Moreover, array's uri must have an s3 prefix.
+        :param ctx: tiledb.Ctx. TileDB Context.
         """
-        self.uri = uri
+        self.namespace = namespace
+        self.ctx = ctx
+
+        # In case we work on TileDB-Cloud we need user's namespace.
+        if self.namespace:
+            s3_prefix = get_s3_prefix(self.namespace)
+            if s3_prefix is None:
+                raise ValueError(
+                    "You must set the default s3 prefix path for ML models in {} profile settings on TileDB-Cloud".format(
+                        self.namespace
+                    )
+                )
+
+            self.uri = "tiledb://{}/{}".format(
+                self.namespace, os.path.join(s3_prefix, uri)
+            )
+        else:
+            self.uri = uri
 
     @abc.abstractmethod
     def save(self, **kwargs):
@@ -30,5 +56,4 @@ class TileDBModel(abc.ABC):
         """
         Abstract method that loads a machine learning model from a model TileDB array.
         Must be implemented per machine learning framework.
-        :return: A Tensorflow or PyTorch model.
         """
