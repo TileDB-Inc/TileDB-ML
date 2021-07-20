df036a830ef896615531d38216a9a587280a9dd1
George Skoumas <george.skoumas@tiledb.com>
2021-07-20 10:45:28 +0300

    Added setuptools_scm to setup.cfg (#71)
    * added setuptools_scm to setup.cfg
    
    * update gitignore for setuptools_scm
    
    * update gitignore for coverage reports
    
    * coverage badge change to psf/black@*

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
 import setuptools
 
-setuptools.setup()
+setuptools.setup(
+    use_scm_version={
+        "version_scheme": "guess-next-dev",
+        "local_scheme": "no-local-version",
+        "write_to": "tiledb/ml/version.py",
+    }
+)
