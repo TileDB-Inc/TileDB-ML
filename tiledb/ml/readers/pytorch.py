1dec4c7d3dc0952a0be3e7ede1d71765fd9143c7
ktsitsi <kostas.t@tiledb.com>
2021-10-01 15:51:15 +0300

    Remove ...

--- tiledb/ml/readers/pytorch.py
+++ tiledb/ml/readers/pytorch.py
@@ -3,13 +3,13 @@
 import math
 from typing import Iterator, Sequence, Tuple
 
-import numpy as np
+# import numpy as np
 import torch
 
 import tiledb
 from tiledb.ml._parallel_utils import run_io_tasks_in_parallel
 
-DataType = Tuple[np.ndarray, ...]
+DataType = Tuple[torch.Tensor, ...]
 
 
 class PyTorchTileDBDenseDataset(torch.utils.data.IterableDataset[DataType]):
