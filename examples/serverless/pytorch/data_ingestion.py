51ebb3941a69272e5e89e2b5dbee5c5ca974b71c
George Skoumas <george.skoumas@tiledb.com>
2021-10-05 14:56:27 +0300

    final PR changes

--- examples/serverless/pytorch/data_ingestion.py
+++ examples/serverless/pytorch/data_ingestion.py
@@ -1,9 +1,8 @@
 import os
 from typing import Any
 
+import numpy as np
 import tiledb.cloud
-
-from tests.utils import ingest_in_tiledb
 
 # Your TileDB username and password, exported as environmental variables
 TILEDB_USER_NAME = os.environ.get("TILEDB_USER_NAME")
@@ -19,6 +18,35 @@ IMAGES_URI = "tiledb://{}/s3://{}/mnist_images".format(TILEDB_NAMESPACE, S3_BUCK
 LABELS_URI = "tiledb://{}/s3://{}/mnist_labels".format(TILEDB_NAMESPACE, S3_BUCKET)
 
 
+# Let's define an ingestion function
+def ingest_in_tiledb(data: np.array, batch_size: int, uri: str) -> None:
+    import tiledb
+
+    # Equal number of dimensions with the numpy array.
+    dims = [
+        tiledb.Dim(
+            name="dim_" + str(dim),
+            domain=(0, data.shape[dim] - 1),
+            tile=data.shape[dim] if dim > 0 else batch_size,
+            dtype=np.int32,
+        )
+        for dim in range(data.ndim)
+    ]
+
+    # TileDB schema
+    schema = tiledb.ArraySchema(
+        domain=tiledb.Domain(*dims),
+        sparse=False,
+        attrs=[tiledb.Attr(name="features", dtype=data.dtype)],
+    )
+    # Create array
+    tiledb.Array.create(uri, schema)
+
+    # Ingest
+    with tiledb.open(uri, "w") as tiledb_array:
+        tiledb_array[:] = {"features": data}
+
+
 def mnist_ingest(ingestion_func: Any) -> None:
     import torchvision
 
@@ -28,14 +56,10 @@ def mnist_ingest(ingestion_func: Any) -> None:
     labels = train_data.targets.numpy()
 
     # Ingest images
-    ingestion_func(
-        data=images, batch_size=64, uri=IMAGES_URI, sparse=False, num_of_attributes=1
-    )
+    ingestion_func(data=images, batch_size=64, uri=IMAGES_URI)
 
     # Ingest labels
-    ingestion_func(
-        data=labels, batch_size=64, uri=LABELS_URI, sparse=False, num_of_attributes=1
-    )
+    ingestion_func(data=labels, batch_size=64, uri=LABELS_URI)
 
 
 tiledb.cloud.login(username=TILEDB_USER_NAME, password=TILEDB_PASSWD)
