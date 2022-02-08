885dfd2de87e6aabbee70e322425fdd3ac3ce726
George Sakkis <george.sakkis@tiledb.com>
2022-02-08 12:12:36 +0200

    Add DenseBatch and SparseBatch helper classes to abstract tensor creation from dense/sparse batches

--- tiledb/ml/readers/_tensorflow_generators.py
+++ tiledb/ml/readers/_tensorflow_generators.py
@@ -13,8 +13,8 @@ Tensor = Union[tf.Tensor, tf.SparseTensor]
 def dense_dense_generator(
     x_array: tiledb.DenseArray,
     y_array: tiledb.DenseArray,
-    x_attribute_names: Sequence[str],
-    y_attribute_names: Sequence[str],
+    x_attrs: Sequence[str],
+    y_attrs: Sequence[str],
     offsets: range,
     batch_size: int,
     buffer_size: int,
@@ -26,8 +26,8 @@ def dense_dense_generator(
 
     :param x_array: An opened TileDB array which contains features.
     :param y_array: An opened TileDB array which contains labels.
-    :param x_attribute_names: Attribute names of x_array.
-    :param y_attribute_names: Attribute names of y_array.
+    :param x_attrs: Attribute names of x_array.
+    :param y_attrs: Attribute names of y_array.
     :param offsets: Row start offsets in x_array/y_array.
     :param batch_size: Size of batch, i.e., number of rows returned per call.
     :param batch_shuffle: True for shuffling batches.
@@ -43,34 +43,24 @@ def dense_dense_generator(
             for batch_offset in _iter_batch_offsets(
                 buffer_size, batch_size, batch_shuffle
             ):
-                x_batch = {
-                    attr: data[batch_offset : batch_offset + batch_size]
-                    for attr, data in x_buffer.items()
-                }
-                y_batch = {
-                    attr: data[batch_offset : batch_offset + batch_size]
-                    for attr, data in y_buffer.items()
-                }
+                batch_slice = slice(batch_offset, batch_offset + batch_size)
+                x_batch = DenseBatch(x_buffer, x_attrs, batch_slice)
+                y_batch = DenseBatch(y_buffer, y_attrs, batch_slice)
 
                 if within_batch_shuffle:
-                    # We get batch length based on the first attribute
-                    # because last batch might be smaller than the batch size
-                    idx = np.arange(x_batch[x_attribute_names[0]].shape[0])
+                    idx = np.arange(len(x_batch))
                     np.random.shuffle(idx)
                 else:
                     idx = Ellipsis
 
-                # Yield the next training batch
-                x_tensors = _get_dense_tensors(x_batch, x_attribute_names, idx)
-                y_tensors = _get_dense_tensors(y_batch, y_attribute_names, idx)
-                yield x_tensors + y_tensors
+                yield x_batch.get_tensors(idx) + y_batch.get_tensors(idx)
 
 
 def sparse_sparse_generator(
     x_array: tiledb.SparseArray,
     y_array: tiledb.SparseArray,
-    x_attribute_names: Sequence[str],
-    y_attribute_names: Sequence[str],
+    x_attrs: Sequence[str],
+    y_attrs: Sequence[str],
     offsets: range,
     batch_size: int,
     buffer_size: int,
@@ -82,8 +72,8 @@ def sparse_sparse_generator(
 
     :param x_array: An opened TileDB array which contains features.
     :param y_array: An opened TileDB array which contains labels.
-    :param x_attribute_names: Attribute names of x_array.
-    :param y_attribute_names: Attribute names of y_array.
+    :param x_attrs: Attribute names of x_array.
+    :param y_attrs: Attribute names of y_array.
     :param offsets: Row start offsets in x_array/y_array.
     :param batch_size: Size of batch, i.e., number of rows returned per call.
     :param batch_shuffle: True for shuffling batches.
@@ -101,44 +91,37 @@ def sparse_sparse_generator(
                 (x_array, y_array),
             )
             # COO to CSR transformation for batching and row slicing
-            x_buffer_csr = _to_csr(x_array, x_attribute_names[0], x_buffer, offset)
-            y_buffer_csr = _to_csr(y_array, y_attribute_names[0], y_buffer, offset)
+            x_buffer_csr = _to_csr(x_array, x_attrs[0], x_buffer, offset)
+            y_buffer_csr = _to_csr(y_array, y_attrs[0], y_buffer, offset)
 
             for batch_offset in _iter_batch_offsets(
                 buffer_size, batch_size, batch_shuffle
             ):
-                x_batch_csr = x_buffer_csr[batch_offset : batch_offset + batch_size]
-                if x_batch_csr.data.size == 0:
+                batch_slice = slice(batch_offset, batch_offset + batch_size)
+                x_batch = SparseBatch(
+                    x_buffer_csr[batch_slice], x_attrs, x_array, x_dense_shape
+                )
+                if not x_batch:
                     return
 
-                y_batch_csr = y_buffer_csr[batch_offset : batch_offset + batch_size]
-
-                # Keep row records number for cross-check between x_array and y_array
-                # batches. Last index excluded shows to empty
-                samples_num_x = np.unique(x_batch_csr.indptr[:-1]).size
-                samples_num_y = np.unique(y_batch_csr.indptr[:-1]).size
-                if samples_num_x != samples_num_y:
+                y_batch = SparseBatch(
+                    y_buffer_csr[batch_slice], y_attrs, y_array, y_dense_shape
+                )
+                if len(x_batch) != len(y_batch):
                     raise ValueError(
                         "x_array and y_array should have the same number of rows, "
                         "i.e. the first dimension of x_array and y_array should be of "
                         "equal domain extent inside the batch"
                     )
 
-                # Transform back to COO for torch.sparse_coo_tensor to digest
-                x_tensors = _get_sparse_tensors(
-                    x_batch_csr, x_array, x_attribute_names, x_dense_shape
-                )
-                y_tensors = _get_sparse_tensors(
-                    y_batch_csr, y_array, y_attribute_names, y_dense_shape
-                )
-                yield x_tensors + y_tensors
+                yield x_batch.get_tensors() + y_batch.get_tensors()
 
 
 def sparse_dense_generator(
     x_array: tiledb.SparseArray,
     y_array: tiledb.DenseArray,
-    x_attribute_names: Sequence[str],
-    y_attribute_names: Sequence[str],
+    x_attrs: Sequence[str],
+    y_attrs: Sequence[str],
     offsets: range,
     batch_size: int,
     buffer_size: int,
@@ -150,8 +133,8 @@ def sparse_dense_generator(
 
     :param x_array: An opened TileDB array which contains features.
     :param y_array: An opened TileDB array which contains labels.
-    :param x_attribute_names: Attribute names of x_array.
-    :param y_attribute_names: Attribute names of y_array.
+    :param x_attrs: Attribute names of x_array.
+    :param y_attrs: Attribute names of y_array.
     :param offsets: Row start offsets in x_array/y_array.
     :param batch_size: Size of batch, i.e., number of rows returned per call.
     :param batch_shuffle: True for shuffling batches.
@@ -168,38 +151,75 @@ def sparse_dense_generator(
                 (x_array, y_array),
             )
             # COO to CSR transformation for batching and row slicing
-            x_buffer_csr = _to_csr(x_array, x_attribute_names[0], x_buffer, offset)
+            x_buffer_csr = _to_csr(x_array, x_attrs[0], x_buffer, offset)
 
             for batch_offset in _iter_batch_offsets(
                 buffer_size, batch_size, batch_shuffle
             ):
-                x_batch_csr = x_buffer_csr[batch_offset : batch_offset + batch_size]
-                if x_batch_csr.data.size == 0:
+                batch_slice = slice(batch_offset, batch_offset + batch_size)
+                x_batch = SparseBatch(
+                    x_buffer_csr[batch_slice], x_attrs, x_array, x_dense_shape
+                )
+                if not x_batch:
                     return
 
-                y_batch = {
-                    attr: data[batch_offset : batch_offset + batch_size]
-                    for attr, data in y_buffer.items()
-                }
-
-                # Keep row records number for cross-check between x_array and y_array
-                # batches. Last index excluded shows to empty
-                samples_num_x = np.unique(x_batch_csr.indptr[:-1]).size
-                # for the check slice the row dimension of y_array dense array
-                samples_num_y = y_batch[y_attribute_names[0]].shape[0]
-                if samples_num_x != samples_num_y:
+                y_batch = DenseBatch(y_buffer, y_attrs, batch_slice)
+                if len(x_batch) != len(y_batch):
                     raise ValueError(
                         "x_array and y_array should have the same number of rows, "
                         "i.e. the first dimension of x_array and y_array should be of "
                         "equal domain extent inside the batch"
                     )
 
-                # Transform back to COO for torch.sparse_coo_tensor to digest
-                x_tensors = _get_sparse_tensors(
-                    x_batch_csr, x_array, x_attribute_names, x_dense_shape
-                )
-                y_tensors = _get_dense_tensors(y_batch, y_attribute_names)
-                yield x_tensors + y_tensors
+                yield x_batch.get_tensors() + y_batch.get_tensors()
+
+
+class DenseBatch:
+    def __init__(
+        self, buffer: Mapping[str, np.ndarray], attrs: Sequence[str], batch_slice: slice
+    ):
+        self._attr_to_batch = {attr: buffer[attr][batch_slice] for attr in attrs}
+
+    def __len__(self) -> int:
+        return len(next(iter(self._attr_to_batch.values())))
+
+    def get_tensors(self, idx: Any = Ellipsis) -> Tuple[tf.Tensor, ...]:
+        return tuple(
+            tf.convert_to_tensor(batch[idx]) for batch in self._attr_to_batch.values()
+        )
+
+
+class SparseBatch:
+    def __init__(
+        self,
+        batch_csr: scipy.sparse.csr_matrix,
+        attrs: Sequence[str],
+        array: tiledb.Array,
+        dense_shape: Tuple[int, ...],
+    ):
+        self._batch_csr = batch_csr
+        self._attrs = attrs
+        self._schema = array.schema
+        self._dense_shape = dense_shape
+
+    def __bool__(self) -> bool:
+        return len(self._batch_csr.data) > 0
+
+    def __len__(self) -> int:
+        # Last index excluded shows to empty
+        return len(np.unique(self._batch_csr.indptr[:-1]))
+
+    def get_tensors(self) -> Tuple[tf.SparseTensor, ...]:
+        batch_coo = self._batch_csr.tocoo()
+        coords = np.stack((batch_coo.row, batch_coo.col), axis=-1)
+        return tuple(
+            tf.SparseTensor(
+                indices=tf.constant(coords, dtype=tf.int64),
+                values=tf.constant(batch_coo.data, dtype=self._schema.attr(attr).dtype),
+                dense_shape=self._dense_shape,
+            )
+            for attr in self._attrs
+        )
 
 
 def _iter_batch_offsets(
@@ -213,33 +233,9 @@ def _iter_batch_offsets(
     return iter(batch_offsets)
 
 
-def _get_dense_tensors(
-    batch: Mapping[str, np.ndarray], attrs: Sequence[str], idx: Any = Ellipsis
-) -> Tuple[tf.Tensor, ...]:
-    return tuple(tf.convert_to_tensor(batch[attr][idx]) for attr in attrs)
-
-
-def _get_sparse_tensors(
-    batch_csr: scipy.sparse.csr_matrix,
-    array: tiledb.SparseArray,
-    attrs: Sequence[str],
-    dense_shape: Tuple[int, ...],
-) -> Tuple[tf.SparseTensor, ...]:
-    batch_coo = batch_csr.tocoo()
-    coords = np.stack((batch_coo.row, batch_coo.col), axis=-1)
-    return tuple(
-        tf.SparseTensor(
-            indices=tf.constant(coords, dtype=tf.int64),
-            values=tf.constant(batch_coo.data, dtype=array.schema.attr(attr).dtype),
-            dense_shape=dense_shape,
-        )
-        for attr in attrs
-    )
-
-
 def _to_csr(
     array: tiledb.Array,
-    attribute_names: str,
+    attr: str,
     buffer: Mapping[str, np.ndarray],
     offset: int,
 ) -> scipy.sparse.csr_matrix:
@@ -259,9 +255,5 @@ def _to_csr(
     row_size_norm = row.max() - row.min() + 1
     col_size_norm = col.max() + 1
     return scipy.sparse.csr_matrix(
-        (
-            buffer[attribute_names],
-            (row - offset, col),
-        ),
-        shape=(row_size_norm, col_size_norm),
+        (buffer[attr], (row - offset, col)), shape=(row_size_norm, col_size_norm)
     )
