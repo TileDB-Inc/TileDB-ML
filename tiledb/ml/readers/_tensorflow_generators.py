0c0e49681fcd137f92f331799aa66e2a659d54b2
George Sakkis <george.sakkis@tiledb.com>
2022-02-08 14:14:27 +0200

    Merge DenseBatchFactory with DenseBatch & SparseBatchFactory with SparseBatch: trade-off OO purity with performance

--- tiledb/ml/readers/_tensorflow_generators.py
+++ tiledb/ml/readers/_tensorflow_generators.py
@@ -34,22 +34,22 @@ def dense_dense_generator(
     :param within_batch_shuffle: True for shuffling records in each batch.
     :return: An iterator of x and y batches.
     """
-    x_batch_factory = DenseBatchFactory(x_attrs)
-    y_batch_factory = DenseBatchFactory(y_attrs)
+    x_batch = DenseBatch(x_attrs)
+    y_batch = DenseBatch(y_attrs)
     with ThreadPoolExecutor(max_workers=2) as executor:
         for offset in offsets:
             x_buffer, y_buffer = executor.map(
                 lambda array: array[offset : offset + buffer_size],  # type: ignore
                 (x_array, y_array),
             )
-            x_batch_factory.set_buffer_offset(x_buffer, offset)
-            y_batch_factory.set_buffer_offset(y_buffer, offset)
+            x_batch.set_buffer_offset(x_buffer, offset)
+            y_batch.set_buffer_offset(y_buffer, offset)
             for batch_offset in _iter_batch_offsets(
                 buffer_size, batch_size, batch_shuffle
             ):
                 batch_slice = slice(batch_offset, batch_offset + batch_size)
-                x_batch = x_batch_factory.get_batch(batch_slice)
-                y_batch = y_batch_factory.get_batch(batch_slice)
+                x_batch.set_batch_slice(batch_slice)
+                y_batch.set_batch_slice(batch_slice)
                 if within_batch_shuffle:
                     idx = np.arange(len(x_batch))
                     np.random.shuffle(idx)
@@ -83,8 +83,8 @@ def sparse_sparse_generator(
     :param within_batch_shuffle: True for shuffling records in each batch.
     :return: An iterator of x and y batches.
     """
-    x_batch_factory = SparseBatchFactory(x_array, x_attrs, batch_size)
-    y_batch_factory = SparseBatchFactory(y_array, y_attrs, batch_size)
+    x_batch = SparseBatch(x_array, x_attrs, batch_size)
+    y_batch = SparseBatch(y_array, y_attrs, batch_size)
     # https://github.com/tensorflow/tensorflow/issues/44565
     with ThreadPoolExecutor(max_workers=2) as executor:
         for offset in offsets:
@@ -92,14 +92,14 @@ def sparse_sparse_generator(
                 lambda array: array[offset : offset + buffer_size],  # type: ignore
                 (x_array, y_array),
             )
-            x_batch_factory.set_buffer_offset(x_buffer, offset)
-            y_batch_factory.set_buffer_offset(y_buffer, offset)
+            x_batch.set_buffer_offset(x_buffer, offset)
+            y_batch.set_buffer_offset(y_buffer, offset)
             for batch_offset in _iter_batch_offsets(
                 buffer_size, batch_size, batch_shuffle
             ):
                 batch_slice = slice(batch_offset, batch_offset + batch_size)
-                x_batch = x_batch_factory.get_batch(batch_slice)
-                y_batch = y_batch_factory.get_batch(batch_slice)
+                x_batch.set_batch_slice(batch_slice)
+                y_batch.set_batch_slice(batch_slice)
                 if len(x_batch) != len(y_batch):
                     raise ValueError(
                         "x_array and y_array should have the same number of rows, "
@@ -134,8 +134,8 @@ def sparse_dense_generator(
     :param within_batch_shuffle: True for shuffling records in each batch.
     :return: An iterator of x and y batches.
     """
-    x_batch_factory = SparseBatchFactory(x_array, x_attrs, batch_size)
-    y_batch_factory = DenseBatchFactory(y_attrs)
+    x_batch = SparseBatch(x_array, x_attrs, batch_size)
+    y_batch = DenseBatch(y_attrs)
     # https://github.com/tensorflow/tensorflow/issues/44565
     with ThreadPoolExecutor(max_workers=2) as executor:
         for offset in offsets:
@@ -143,14 +143,14 @@ def sparse_dense_generator(
                 lambda array: array[offset : offset + buffer_size],  # type: ignore
                 (x_array, y_array),
             )
-            x_batch_factory.set_buffer_offset(x_buffer, offset)
-            y_batch_factory.set_buffer_offset(y_buffer, offset)
+            x_batch.set_buffer_offset(x_buffer, offset)
+            y_batch.set_buffer_offset(y_buffer, offset)
             for batch_offset in _iter_batch_offsets(
                 buffer_size, batch_size, batch_shuffle
             ):
                 batch_slice = slice(batch_offset, batch_offset + batch_size)
-                x_batch = x_batch_factory.get_batch(batch_slice)
-                y_batch = y_batch_factory.get_batch(batch_slice)
+                x_batch.set_batch_slice(batch_slice)
+                y_batch.set_batch_slice(batch_slice)
                 if len(x_batch) != len(y_batch):
                     raise ValueError(
                         "x_array and y_array should have the same number of rows, "
@@ -162,78 +162,44 @@ def sparse_dense_generator(
 
 
 class DenseBatch:
-    def __init__(
-        self, buffer: Mapping[str, np.ndarray], attrs: Sequence[str], batch_slice: slice
-    ):
-        self._attr_to_batch = {attr: buffer[attr][batch_slice] for attr in attrs}
-
-    def __len__(self) -> int:
-        return len(next(iter(self._attr_to_batch.values())))
-
-    def get_tensors(self, idx: Any = Ellipsis) -> Tuple[tf.Tensor, ...]:
-        return tuple(
-            tf.convert_to_tensor(batch[idx]) for batch in self._attr_to_batch.values()
-        )
-
-
-class DenseBatchFactory:
     def __init__(self, attrs: Sequence[str]):
         self._attrs = attrs
 
     def set_buffer_offset(self, buffer: Mapping[str, np.ndarray], offset: int) -> None:
         self._buffer = buffer
 
-    def get_batch(self, batch_slice: slice) -> DenseBatch:
-        return DenseBatch(self._buffer, self._attrs, batch_slice)
+    def set_batch_slice(self, batch_slice: slice) -> None:
+        if not hasattr(self, "_buffer"):
+            raise RuntimeError("set_buffer_offset() not called")
+        self._batch = {attr: self._buffer[attr][batch_slice] for attr in self._attrs}
+
+    def get_tensors(self, idx: Any = Ellipsis) -> Tuple[tf.Tensor, ...]:
+        if not hasattr(self, "_batch"):
+            raise RuntimeError("set_batch_slice() not called")
+        return tuple(
+            tf.convert_to_tensor(attr_batch[idx]) for attr_batch in self._batch.values()
+        )
+
+    def __len__(self) -> int:
+        if not hasattr(self, "_batch"):
+            raise RuntimeError("set_batch_slice() not called")
+        return len(next(iter(self._batch.values())))
 
 
 class SparseBatch:
-    def __init__(
-        self,
-        batch_csr: scipy.sparse.csr_matrix,
-        array: tiledb.Array,
-        attrs: Sequence[str],
-        dense_shape: Tuple[int, ...],
-    ):
-        self._batch_csr = batch_csr
-        self._schema = array.schema
-        self._attrs = attrs
-        self._dense_shape = dense_shape
-
-    def __bool__(self) -> bool:
-        return len(self._batch_csr.data) > 0
-
-    def __len__(self) -> int:
-        # return number of non-zero rows
-        return int((self._batch_csr.getnnz(axis=1) > 0).sum())
-
-    def get_tensors(self) -> Tuple[tf.SparseTensor, ...]:
-        batch_coo = self._batch_csr.tocoo()
-        coords = np.stack((batch_coo.row, batch_coo.col), axis=-1)
-        return tuple(
-            tf.SparseTensor(
-                indices=tf.constant(coords, dtype=tf.int64),
-                values=tf.constant(batch_coo.data, dtype=self._schema.attr(attr).dtype),
-                dense_shape=self._dense_shape,
-            )
-            for attr in self._attrs
-        )
-
-
-class SparseBatchFactory:
     def __init__(
         self,
         array: tiledb.SparseArray,
         attrs: Sequence[str],
         batch_size: int,
     ):
-        self._array = array
+        self._schema = array.schema
         self._attrs = attrs
         self._dense_shape = (batch_size, array.schema.domain.shape[1])
 
     def set_buffer_offset(self, buffer: Mapping[str, np.ndarray], offset: int) -> None:
         # COO to CSR transformation for batching and row slicing
-        dim = self._array.schema.domain.dim
+        dim = self._schema.domain.dim
         row = buffer[dim(0).name]
         col = buffer[dim(1).name]
         # Normalize indices: We want the coords indices to be in the [0, batch_size]
@@ -246,10 +212,35 @@ class SparseBatchFactory:
             shape=(row_size_norm, col_size_norm),
         )
 
-    def get_batch(self, batch_slice: slice) -> SparseBatch:
-        return SparseBatch(
-            self._buffer_csr[batch_slice], self._array, self._attrs, self._dense_shape
+    def set_batch_slice(self, batch_slice: slice) -> None:
+        if not hasattr(self, "_buffer_csr"):
+            raise RuntimeError("set_buffer_offset() not called")
+        self._batch_csr = self._buffer_csr[batch_slice]
+
+    def get_tensors(self) -> Tuple[tf.SparseTensor, ...]:
+        if not hasattr(self, "_batch_csr"):
+            raise RuntimeError("set_batch_slice() not called")
+        batch_coo = self._batch_csr.tocoo()
+        coords = np.stack((batch_coo.row, batch_coo.col), axis=-1)
+        return tuple(
+            tf.SparseTensor(
+                indices=tf.constant(coords, dtype=tf.int64),
+                values=tf.constant(batch_coo.data, dtype=self._schema.attr(attr).dtype),
+                dense_shape=self._dense_shape,
+            )
+            for attr in self._attrs
         )
+
+    def __len__(self) -> int:
+        if not hasattr(self, "_batch_csr"):
+            raise RuntimeError("set_batch_slice() not called")
+        # return number of non-zero rows
+        return int((self._batch_csr.getnnz(axis=1) > 0).sum())
+
+    def __bool__(self) -> bool:
+        if not hasattr(self, "_batch_csr"):
+            raise RuntimeError("set_batch_slice() not called")
+        return len(self._batch_csr.data) > 0
 
 
 def _iter_batch_offsets(
