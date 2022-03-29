43f39bd22e0fff74bc5e3b5f806b00c922ee959e
George Sakkis <george.sakkis@tiledb.com>
2022-03-29 14:26:43 +0300

    Implement shuffle_buffer_size for PyTorchTileDBDataset

--- tiledb/ml/readers/pytorch.py
+++ tiledb/ml/readers/pytorch.py
@@ -1,7 +1,9 @@
 """Functionality for loading data from TileDB arrays to the PyTorch Dataloader API."""
 
+import itertools as it
 import math
-from typing import Iterator, Optional, Sequence
+import random
+from typing import Iterable, Iterator, Optional, Sequence, TypeVar
 
 import numpy as np
 import torch
@@ -33,7 +35,9 @@ def PyTorchTileDBDataLoader(
     :param y_attrs: Attribute names of y_array.
     :param num_workers: how many subprocesses to use for data loading
     """
-    dataset = PyTorchTileDBDataset(x_array, y_array, buffer_bytes, x_attrs, y_attrs)
+    dataset = PyTorchTileDBDataset(
+        x_array, y_array, buffer_bytes, shuffle_buffer_size, x_attrs, y_attrs
+    )
     return torch.utils.data.DataLoader(
         dataset, batch_size=batch_size, num_workers=num_workers
     )
@@ -45,6 +49,7 @@ class PyTorchTileDBDataset(torch.utils.data.IterableDataset[Sequence[torch.Tenso
         x_array: tiledb.Array,
         y_array: tiledb.Array,
         buffer_bytes: Optional[int] = None,
+        shuffle_buffer_size: int = 0,
         x_attrs: Sequence[str] = (),
         y_attrs: Sequence[str] = (),
     ):
@@ -54,6 +59,7 @@ class PyTorchTileDBDataset(torch.utils.data.IterableDataset[Sequence[torch.Tenso
             raise ValueError("X and Y arrays must have the same number of rows")
 
         self._rows = rows
+        self._shuffle_buffer_size = shuffle_buffer_size
         self._generator_kwargs = dict(
             x_array=x_array,
             y_array=y_array,
@@ -64,7 +70,7 @@ class PyTorchTileDBDataset(torch.utils.data.IterableDataset[Sequence[torch.Tenso
         )
 
     def __iter__(self) -> Iterator[Sequence[torch.Tensor]]:
-        kwargs = self._generator_kwargs.copy()
+        kwargs = self._generator_kwargs
         worker_info = torch.utils.data.get_worker_info()
         if worker_info is not None:
             for array_key in "x_array", "y_array":
@@ -75,9 +81,39 @@ class PyTorchTileDBDataset(torch.utils.data.IterableDataset[Sequence[torch.Tenso
             per_worker = int(math.ceil(self._rows / worker_info.num_workers))
             start_offset = worker_info.id * per_worker
             stop_offset = min(start_offset + per_worker, self._rows)
-            kwargs.update(start_offset=start_offset, stop_offset=stop_offset)
-        for batch_tensors in tensor_generator(**kwargs):
-            yield from zip(*batch_tensors)
+            kwargs = dict(start_offset=start_offset, stop_offset=stop_offset, **kwargs)
+
+        rows: Iterator[Sequence[torch.Tensor]] = (
+            row
+            for batch_tensors in tensor_generator(**kwargs)
+            for row in zip(*batch_tensors)
+        )
+        if self._shuffle_buffer_size > 0:
+            rows = iter_shuffled(rows, self._shuffle_buffer_size)
+        return rows
+
+
+T = TypeVar("T")
+
+
+def iter_shuffled(iterable: Iterable[T], buffer_size: int) -> Iterator[T]:
+    """
+    Shuffle the given iterable with a buffer.
+
+    The buffer with `buffer_size` is filled with elements from the iterable first.
+    Then, each item will be yielded from the buffer by reservoir sampling via iterator.
+
+    """
+    iterator = iter(iterable)
+    buffer = list(it.islice(iterator, buffer_size))
+    randrange = random.randrange
+    for x in iterator:
+        idx = randrange(0, buffer_size)
+        yield buffer[idx]
+        buffer[idx] = x
+    random.shuffle(buffer)
+    while buffer:
+        yield buffer.pop()
 
 
 class PyTorchSparseTileDBTensorGenerator(SparseTileDBTensorGenerator[torch.Tensor]):
