68811840d6effe40745d8945c1baa034df80c66b
George Skoumas <george.skoumas@tiledb.com>
2022-04-15 13:42:26 +0300

    changed setuptools_scm tag scheme, add Python 3.7 to ci.yml

--- setup.py
+++ setup.py
@@ -1,17 +1,9 @@
-from importlib.metadata import PackageNotFoundError, version
-
 import setuptools
 
 setuptools.setup(
     use_scm_version={
         "version_scheme": "guess-next-dev",
-        "local_scheme": "no-local-version",
+        "local_scheme": "dirty-tag",
         "write_to": "tiledb/ml/version.py",
     }
 )
-
-try:
-    __version__ = version("package-name")
-except PackageNotFoundError:
-    # package is not installed
-    pass
