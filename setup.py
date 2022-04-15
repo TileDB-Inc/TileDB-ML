7687ac7f75c69bfcf4f85d3bd6b438e9be5dd2dc
George Skoumas <george.skoumas@tiledb.com>
2022-04-15 15:31:29 +0300

    moved __version__ code to tiledb/ml/__init__.py

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
 import setuptools
-from pkg_resources import DistributionNotFound, get_distribution  # type: ignore
-
-# TODO change pkg_resources with importlib.metadata, as described here
-#  https://pypi.org/project/setuptools-scm/#:~:text=Retrieving%20package%20version%20at%20runtime, when we stop
-#  supporting Python 3.7. We the aforementioned change, we can avoid the 100ms overhead during import of the package.
 
 setuptools.setup(
     use_scm_version={
@@ -12,9 +7,3 @@ setuptools.setup(
         "write_to": "tiledb/ml/version.py",
     }
 )
-
-try:
-    __version__ = get_distribution("tiledb-ml").version
-except DistributionNotFound:
-    # package is not installed
-    pass
