b2b204c6e792c2e61d8a22b844ba357941381cca
George Sakkis <george.sakkis@tiledb.com>
2022-04-28 14:38:48 +0300

    Relax requirements

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 import setuptools
 
-tensorflow = ["tensorflow<=2.5.0"]
-pytorch = ["torch>=1.8.1, <1.10"]
-sklearn = ["scikit-learn>=0.23.0"]
-cloud = ["tiledb-cloud>=0.7.11"]
-full = sorted({"torchvision>=0.9.1", *tensorflow, *pytorch, *sklearn, *cloud})
+tensorflow = ["tensorflow>=2.4.1"]
+pytorch = ["torch>=1.8.1"]
+sklearn = ["scikit-learn>=1.0"]
+cloud = ["tiledb-cloud"]
+full = sorted({"torchvision", *tensorflow, *pytorch, *sklearn, *cloud})
 
 setuptools.setup(
-    setup_requires=["setuptools_scm <= 6.0"],
+    setup_requires=["setuptools_scm"],
     use_scm_version={
         "version_scheme": "guess-next-dev",
         "local_scheme": "dirty-tag",
         "write_to": "tiledb/ml/version.py",
     },
-    install_requires=["sparse", "tiledb >= 0.14"],
+    install_requires=["sparse", "tiledb>=0.14"],
     extras_require={
         "tensorflow": tensorflow,
         "pytorch": pytorch,
