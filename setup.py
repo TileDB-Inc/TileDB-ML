ac62574b25bf3c98ad7ef08e8320df1338af4f27
George Sakkis <george.sakkis@tiledb.com>
2022-04-28 13:24:03 +0300

    Move requirements from setup.cfg to setup.py, keep torchvision only for the 'full' optional dependency

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 import setuptools
 
+tensorflow = ["tensorflow<=2.5.0"]
+pytorch = ["torch>=1.8.1, <1.10"]
+sklearn = ["scikit-learn>=0.23.0"]
+cloud = ["tiledb-cloud>=0.7.11"]
+full = sorted({"torchvision>=0.9.1", *tensorflow, *pytorch, *sklearn, *cloud})
+
 setuptools.setup(
+    setup_requires=["setuptools_scm <= 6.0"],
     use_scm_version={
         "version_scheme": "guess-next-dev",
         "local_scheme": "dirty-tag",
         "write_to": "tiledb/ml/version.py",
-    }
+    },
+    install_requires=["sparse", "tiledb >= 0.14"],
+    extras_require={
+        "tensorflow": tensorflow,
+        "pytorch": pytorch,
+        "sklearn": sklearn,
+        "cloud": cloud,
+        "full": full,
+    },
 )
