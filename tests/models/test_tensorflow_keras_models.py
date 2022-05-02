53ef0bec0aaaeb344f79ddc8e60e71e1fb600c79
George Sakkis <george.sakkis@tiledb.com>
2022-05-02 12:04:04 +0300

    Removed (almost) all tensorflow.python imports from test_tensorflow_keras_models.py

--- tests/models/test_tensorflow_keras_models.py
+++ tests/models/test_tensorflow_keras_models.py
@@ -9,13 +9,7 @@ import shutil
 import numpy as np
 import pytest
 import tensorflow as tf
-from tensorflow.python import keras
-from tensorflow.python.feature_column import feature_column_lib
-from tensorflow.python.framework import sparse_tensor
-from tensorflow.python.keras import optimizers, testing_utils
-from tensorflow.python.keras.backend import batch_get_value
-from tensorflow.python.keras.feature_column import dense_features
-from tensorflow.python.keras.feature_column import sequence_feature_column as ksfc
+from tensorflow.python.keras import testing_utils
 
 import tiledb
 from tiledb.ml.models.tensorflow_keras import TensorflowKerasTileDBModel
@@ -23,12 +17,14 @@ from tiledb.ml.models.tensorflow_keras import TensorflowKerasTileDBModel
 # Suppress all Tensorflow messages
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
+batch_get_value = tf.keras.backend.batch_get_value
 
-class ConfigSubclassModel(keras.Model):
+
+class ConfigSubclassModel(tf.keras.Model):
     def __init__(self, hidden_units):
         super(ConfigSubclassModel, self).__init__()
         self.hidden_units = hidden_units
-        self.dense_layers = [keras.layers.Dense(u) for u in hidden_units]
+        self.dense_layers = [tf.keras.layers.Dense(u) for u in hidden_units]
 
     def call(self, inputs):
         x = inputs
@@ -55,12 +51,8 @@ loss_optimizer_metrics = pytest.mark.parametrize(
     [
         (None, None, None),
         ("binary_crossentropy", "rmsprop", "accuracy"),
-        (keras.losses.MSE, "rmsprop", keras.metrics.categorical_accuracy),
-        (
-            keras.losses.SparseCategoricalCrossentropy(),
-            optimizers.gradient_descent_v2.SGD(),
-            keras.metrics.SparseCategoricalCrossentropy(),
-        ),
+        ("mean_squared_error", "rmsprop", "categorical_accuracy"),
+        ("sparse_categorical_crossentropy", "sgd", "sparse_categorical_crossentropy"),
     ],
 )
 
@@ -170,10 +162,8 @@ class TestTensorflowKerasModel:
         data = np.random.rand(100, input_shape[-1] if api == ConfigSubclassModel else 3)
 
         if optimizer:
-            model_opt_weights = batch_get_value(getattr(model.optimizer, "weights"))
-            loaded_opt_weights = batch_get_value(
-                getattr(loaded_model.optimizer, "weights")
-            )
+            model_opt_weights = batch_get_value(model.optimizer.weights)
+            loaded_opt_weights = batch_get_value(loaded_model.optimizer.weights)
 
             # Assert optimizer weights are equal
             for weight_model, weight_loaded_model in zip(
@@ -191,36 +181,32 @@ class TestTensorflowKerasModel:
         if optimizer is None:
             pytest.skip()
         cols = [
-            feature_column_lib.numeric_column("a"),
-            feature_column_lib.indicator_column(
-                feature_column_lib.categorical_column_with_vocabulary_list(
+            tf.feature_column.numeric_column("a"),
+            tf.feature_column.indicator_column(
+                tf.feature_column.categorical_column_with_vocabulary_list(
                     "b", ["one", "two"]
                 )
             ),
         ]
         input_layers = {
-            "a": keras.layers.Input(shape=(1,), name="a"),
-            "b": keras.layers.Input(shape=(1,), name="b", dtype="string"),
+            "a": tf.keras.layers.Input(shape=(1,), name="a"),
+            "b": tf.keras.layers.Input(shape=(1,), name="b", dtype="string"),
         }
 
-        fc_layer = dense_features.DenseFeatures(cols)(input_layers)
-        output = keras.layers.Dense(10)(fc_layer)
+        fc_layer = tf.keras.layers.DenseFeatures(cols)(input_layers)
+        output = tf.keras.layers.Dense(10)(fc_layer)
 
-        model = keras.models.Model(input_layers, output)
+        model = tf.keras.Model(input_layers, output)
 
-        model.compile(
-            loss=loss,
-            optimizer=optimizer,
-            metrics=[metrics],
-        )
+        model.compile(loss=loss, optimizer=optimizer, metrics=[metrics])
 
         tiledb_uri = os.path.join(tmpdir, "model_array")
         tiledb_model_obj = TensorflowKerasTileDBModel(uri=tiledb_uri, model=model)
         tiledb_model_obj.save(include_optimizer=True)
         loaded_model = tiledb_model_obj.load(compile_model=True)
 
-        model_opt_weights = batch_get_value(getattr(model.optimizer, "weights"))
-        loaded_opt_weights = batch_get_value(getattr(loaded_model.optimizer, "weights"))
+        model_opt_weights = batch_get_value(model.optimizer.weights)
+        loaded_opt_weights = batch_get_value(loaded_model.optimizer.weights)
 
         # Assert optimizer weights are equal
         for weight_model, weight_loaded_model in zip(
@@ -243,39 +229,35 @@ class TestTensorflowKerasModel:
             pytest.skip()
 
         cols = [
-            feature_column_lib.sequence_numeric_column("a"),
-            feature_column_lib.indicator_column(
-                feature_column_lib.sequence_categorical_column_with_vocabulary_list(
+            tf.feature_column.sequence_numeric_column("a"),
+            tf.feature_column.indicator_column(
+                tf.feature_column.sequence_categorical_column_with_vocabulary_list(
                     "b", ["one", "two"]
                 )
             ),
         ]
         input_layers = {
-            "a": keras.layers.Input(shape=(None, 1), sparse=True, name="a"),
-            "b": keras.layers.Input(
+            "a": tf.keras.layers.Input(shape=(None, 1), sparse=True, name="a"),
+            "b": tf.keras.layers.Input(
                 shape=(None, 1), sparse=True, name="b", dtype="string"
             ),
         }
 
-        fc_layer, _ = ksfc.SequenceFeatures(cols)(input_layers)
-        x = keras.layers.GRU(32)(fc_layer)
-        output = keras.layers.Dense(10)(x)
+        fc_layer, _ = tf.keras.experimental.SequenceFeatures(cols)(input_layers)
+        x = tf.keras.layers.GRU(32)(fc_layer)
+        output = tf.keras.layers.Dense(10)(x)
 
-        model = keras.models.Model(input_layers, output)
+        model = tf.keras.Model(input_layers, output)
 
-        model.compile(
-            loss=loss,
-            optimizer=optimizer,
-            metrics=[metrics],
-        )
+        model.compile(loss=loss, optimizer=optimizer, metrics=[metrics])
 
         tiledb_uri = os.path.join(tmpdir, "model_array")
         tiledb_model_obj = TensorflowKerasTileDBModel(uri=tiledb_uri, model=model)
         tiledb_model_obj.save(include_optimizer=True)
         loaded_model = tiledb_model_obj.load(compile_model=True)
 
-        model_opt_weights = batch_get_value(getattr(model.optimizer, "weights"))
-        loaded_opt_weights = batch_get_value(getattr(loaded_model.optimizer, "weights"))
+        model_opt_weights = batch_get_value(model.optimizer.weights)
+        loaded_opt_weights = batch_get_value(loaded_model.optimizer.weights)
 
         # Assert optimizer weights are equal
         for weight_model, weight_loaded_model in zip(
@@ -289,16 +271,12 @@ class TestTensorflowKerasModel:
         values_a = np.arange(10, dtype=np.float32)
         indices_a = np.zeros((10, 3), dtype=np.int64)
         indices_a[:, 0] = np.arange(10)
-        inputs_a = sparse_tensor.SparseTensor(
-            indices_a, values_a, (batch_size, timesteps, 1)
-        )
+        inputs_a = tf.SparseTensor(indices_a, values_a, (batch_size, timesteps, 1))
 
         values_b = np.zeros(10, dtype=np.str)
         indices_b = np.zeros((10, 3), dtype=np.int64)
         indices_b[:, 0] = np.arange(10)
-        inputs_b = sparse_tensor.SparseTensor(
-            indices_b, values_b, (batch_size, timesteps, 1)
-        )
+        inputs_b = tf.SparseTensor(indices_b, values_b, (batch_size, timesteps, 1))
 
         # Assert model predictions are equal
         np.testing.assert_array_equal(
@@ -307,18 +285,18 @@ class TestTensorflowKerasModel:
         )
 
     def test_functional_model_save_load_with_custom_loss_and_metric(self, tmpdir):
-        inputs = keras.Input(shape=(4,))
-        x = keras.layers.Dense(8, activation="relu")(inputs)
-        outputs = keras.layers.Dense(3, activation="softmax")(x)
-        model = keras.Model(inputs=inputs, outputs=outputs)
-        custom_loss = keras.layers.Lambda(lambda x: keras.backend.sum(x * x))(x)
+        inputs = tf.keras.layers.Input(shape=(4,))
+        x = tf.keras.layers.Dense(8, activation="relu")(inputs)
+        outputs = tf.keras.layers.Dense(3, activation="softmax")(x)
+        model = tf.keras.Model(inputs=inputs, outputs=outputs)
+        custom_loss = tf.keras.layers.Lambda(lambda x: tf.keras.backend.sum(x * x))(x)
         model.add_loss(custom_loss)
         model.add_metric(custom_loss, aggregation="mean", name="custom_loss")
 
         model.compile(
-            loss=keras.losses.SparseCategoricalCrossentropy(),
-            optimizer=optimizers.gradient_descent_v2.SGD(),
-            metrics=[keras.metrics.SparseCategoricalCrossentropy()],
+            loss="sparse_categorical_crossentropy",
+            optimizer="sgd",
+            metrics=["sparse_categorical_crossentropy"],
         )
 
         data_x = np.random.normal(size=(32, 4))
@@ -347,17 +325,17 @@ class TestTensorflowKerasModel:
         )
 
     def test_save_load_for_rnn_layers(self, tmpdir):
-        inputs = keras.Input([10, 10], name="train_input")
+        inputs = tf.keras.layers.Input([10, 10], name="train_input")
         rnn_layers = [
-            keras.layers.LSTMCell(size, recurrent_dropout=0, name="rnn_cell%d" % i)
+            tf.keras.layers.LSTMCell(size, recurrent_dropout=0, name="rnn_cell%d" % i)
             for i, size in enumerate([32, 32])
         ]
-        rnn_output = keras.layers.RNN(
+        rnn_output = tf.keras.layers.RNN(
             rnn_layers, return_sequences=True, name="rnn_layer"
         )(inputs)
-        pred_feat = keras.layers.Dense(10, name="prediction_features")(rnn_output)
-        pred = keras.layers.Softmax()(pred_feat)
-        model = keras.Model(inputs=[inputs], outputs=[pred, pred_feat])
+        pred_feat = tf.keras.layers.Dense(10, name="prediction_features")(rnn_output)
+        pred = tf.keras.layers.Softmax()(pred_feat)
+        model = tf.keras.Model(inputs=[inputs], outputs=[pred, pred_feat])
 
         tiledb_uri = os.path.join(tmpdir, "model_array")
         tiledb_model_obj = TensorflowKerasTileDBModel(uri=tiledb_uri, model=model)
@@ -370,15 +348,15 @@ class TestTensorflowKerasModel:
         np.testing.assert_array_equal(loaded_model.predict(data), model.predict(data))
 
     def test_sequential_model_save_load_without_input_shape(self, tmpdir):
-        model = keras.models.Sequential()
-        model.add(keras.layers.Dense(2))
-        model.add(keras.layers.RepeatVector(3))
-        model.add(keras.layers.TimeDistributed(keras.layers.Dense(3)))
+        model = tf.keras.Sequential()
+        model.add(tf.keras.layers.Dense(2))
+        model.add(tf.keras.layers.RepeatVector(3))
+        model.add(tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(3)))
         model.compile(
-            loss=keras.losses.MSE,
+            loss="mean_squared_error",
             optimizer="rmsprop",
-            metrics=keras.metrics.categorical_accuracy,
-            weighted_metrics=keras.metrics.categorical_accuracy,
+            metrics="categorical_accuracy",
+            weighted_metrics="categorical_accuracy",
             sample_weight_mode="temporal",
         )
         data_x = np.random.random((1, 3))
@@ -432,8 +410,8 @@ class TestTensorflowKerasModel:
 
 class TestTensorflowKerasModelCloud:
     def test_file_properties(self, tmpdir):
-        model = keras.models.Sequential()
-        model.add(keras.layers.Flatten(input_shape=(10, 10)))
+        model = tf.keras.Sequential()
+        model.add(tf.keras.layers.Flatten(input_shape=(10, 10)))
 
         # Get model summary in a string
         s = io.StringIO()
@@ -459,8 +437,8 @@ class TestTensorflowKerasModelCloud:
         assert tiledb_obj._file_properties["TILEDB_ML_MODEL_PREVIEW"] == model_summary
 
     def test_get_cloud_uri_call_for_models_on_tiledb_cloud(self, tmpdir, mocker):
-        model = keras.models.Sequential()
-        model.add(keras.layers.Flatten(input_shape=(10, 10)))
+        model = tf.keras.Sequential()
+        model.add(tf.keras.layers.Flatten(input_shape=(10, 10)))
         uri = os.path.join(tmpdir, "model_array")
 
         mock_get_cloud_uri = mocker.patch(
@@ -472,8 +450,8 @@ class TestTensorflowKerasModelCloud:
         mock_get_cloud_uri.assert_called_once_with(uri, "test_namespace")
 
     def test_get_s3_prefix_call_for_models_on_tiledb_cloud(self, tmpdir, mocker):
-        model = keras.models.Sequential()
-        model.add(keras.layers.Flatten(input_shape=(10, 10)))
+        model = tf.keras.Sequential()
+        model.add(tf.keras.layers.Flatten(input_shape=(10, 10)))
         uri = os.path.join(tmpdir, "model_array")
 
         mock_get_s3_prefix = mocker.patch(
@@ -485,8 +463,8 @@ class TestTensorflowKerasModelCloud:
         mock_get_s3_prefix.assert_called_once_with("test_namespace")
 
     def test_update_file_properties_call(self, tmpdir, mocker):
-        model = keras.models.Sequential()
-        model.add(keras.layers.Flatten(input_shape=(10, 10)))
+        model = tf.keras.Sequential()
+        model.add(tf.keras.layers.Flatten(input_shape=(10, 10)))
 
         # Get model summary in a string
         s = io.StringIO()
@@ -522,8 +500,8 @@ class TestTensorflowKerasModelCloud:
         mock_update_file_properties.assert_called_once_with(uri, file_properties_dict)
 
     def test_exception_raise_file_property_in_meta_error(self, tmpdir):
-        model = keras.models.Sequential()
-        model.add(keras.layers.Flatten(input_shape=(10, 10)))
+        model = tf.keras.Sequential()
+        model.add(tf.keras.layers.Flatten(input_shape=(10, 10)))
         tiledb_array = os.path.join(tmpdir, "model_array")
         tiledb_obj = TensorflowKerasTileDBModel(uri=tiledb_array, model=model)
         with pytest.raises(ValueError) as ex:
@@ -536,8 +514,8 @@ class TestTensorflowKerasModelCloud:
         )
 
     def test_tensorboard_callback_meta(self, tmpdir):
-        model = keras.models.Sequential()
-        model.add(keras.layers.Flatten(input_shape=(10, 10)))
+        model = tf.keras.Sequential()
+        model.add(tf.keras.layers.Flatten(input_shape=(10, 10)))
         tiledb_array = os.path.join(tmpdir, "model_array")
         tiledb_obj = TensorflowKerasTileDBModel(uri=tiledb_array, model=model)
 
